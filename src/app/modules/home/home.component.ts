07dddced466ac4f854cfce0c46c7d3a36f90437d
Carlos Mafra <carlosfgmafra@gmail.com>
2025-04-03 21:15:06 -0400

    [FEAT] - Gerando tela de loading

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {CardComponent} from '../card/card.component';
 import {PokemonService} from '../../services/pokemon.service';
 import {PokemonListItem} from '../../shared/interface/pokemon-list-item';
@@ -8,6 +8,7 @@ import {Button} from 'primeng/button';
 import {InputGroup} from 'primeng/inputgroup';
 import {InputText} from 'primeng/inputtext';
 import {PokemonDetails} from '../../shared/interface/pokemon-details';
+import {LoadingComponent} from '../../shared/modules/loading/loading.component';
 
 @Component({
   selector: 'app-home',
@@ -17,7 +18,8 @@ import {PokemonDetails} from '../../shared/interface/pokemon-details';
     FormsModule,
     Button,
     InputGroup,
-    InputText
+    InputText,
+    LoadingComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
@@ -26,21 +28,32 @@ export class HomeComponent implements OnInit {
   public pokemons: PokemonListItem[] = [];
   public service: PokemonService = inject(PokemonService)
   public pokemon: string = "";
+  public isLoading = signal<boolean>(false);
 
   ngOnInit() {
     this.getAllPokemons().then();
   }
 
   public async getAllPokemons() {
+    this.isLoading.set(true)
     await this.service.getAllPokemon(151, 0).then((res: PokemonListResponse) => {
       this.pokemons = res.results
+    }).catch(() => {
+
+    }).finally(() => {
+      this.isLoading.set(false)
+
     })
   }
 
   public async buscar() {
+    this.isLoading.set(true);
+
     if (this.pokemon.trim()) {
       try {
-        const pokemonDetails: PokemonDetails = await this.service.getByIdPokemon(this.pokemon.toLowerCase());
+        const pokemonDetails: PokemonDetails = await this.service.getByIdPokemon(this.pokemon.toLowerCase()).finally(() => {
+          this.isLoading.set(false)
+        });
         this.pokemons = [pokemonDetails.species]
       } catch (error) {
         console.error('Pokémon não encontrado:', error);
